946e27eb3279698777d7aad03765211a987ad720
VInicius Maciel <vm.viniciusmaciel@gmail.com>
2022-10-03 16:23:52 -0300

    The list of questions to display is now separeted from the QuestionList component

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -2,34 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import Question from "./Question";
 import CardAnswer from "./CardAnswer";
-
-const questions = [
-  // { question: "", answer: "" },
-  {
-    question: "O que é JSX?",
-    answer: "Uma extensão de linguagem do JavaScript",
-    status: "",
-  },
-  {
-    question: "O React é __",
-    answer: "Uma biblioteca JavaScript para construção de interfaces",
-    status: "",
-  },
-  {
-    question: "Componentes devem iniciar com __",
-    answer: "letra maiúscula",
-    status: "",
-  },
-
-  {
-    question: "O ReactDOM nos ajuda __",
-    answer: "interagindo com a DOM para colocar componentes React na mesma",
-    status: "",
-  },
-  { question: "Teste", answer: "Testando", status: "" },
-  { question: "Teste", answer: "Testando", status: "" },
-  { question: "Teste", answer: "Testando", status: "" },
-];
+import questions from "../getQuestions";
 
 function QuestionList() {
   const [questionList, setQuestionList] = React.useState(questions);
---
+++ src/getQuestions.js
@@ -0,0 +1,41 @@
+const questions = [
+  // { question: "", answer: "" },
+  {
+    question: "O que é JSX?",
+    answer: "Uma extensão de linguagem do JavaScript",
+    status: "",
+  },
+  {
+    question: "O React é __",
+    answer: "Uma biblioteca JavaScript para construção de interfaces",
+    status: "",
+  },
+  {
+    question: "Componentes devem iniciar com __",
+    answer: "letra maiúscula",
+    status: "",
+  },
+
+  {
+    question: "O ReactDOM nos ajuda __",
+    answer: "interagindo com a DOM para colocar componentes React na mesma",
+    status: "",
+  },
+  {
+    question: "Podemos colocar __ dentro do JSX",
+    answer: "interagindo com a DOM para colocar componentes React na mesma",
+    status: "",
+  },
+  {
+    question: " Usamos o npm para __",
+    answer: "gerenciar os pacotes necessários e suas dependências",
+    status: "",
+  },
+  {
+    question: "Usamos props para __",
+    answer: "passar diferentes informações para componentes ",
+    status: "",
+  },
+];
+
+export default questions;
